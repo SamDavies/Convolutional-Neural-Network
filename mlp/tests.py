be450fd902d3cddd01d2cdc4b3bf974c9a7b0135
Sam Davies <samdavies@live.co.uk>
2015-10-17 19:13:54 +0100

    Test sigmoid forward prop

--- mlp/tests.py
+++ mlp/tests.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
 
+import numpy
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
 from mlp.layers import Sigmoid
 
 __author__ = 'Sam Davies'
@@ -20,3 +23,20 @@ class SigmoidTestCase(TestCase):
         """ Ensure that a zero value gives the correct sigmoid value """
         actual = Sigmoid.sigmoid(0.0)
         self.assertAlmostEqual(0.5, actual)
+
+    def test_sigmoid_forward_propagation(self):
+        """ Ensure that a sigmoid layer forwards propagates correctly """
+        # Given
+        rng = numpy.random.RandomState([2015, 10, 10])
+        rng_state = rng.get_state()
+        input_layer = numpy.asarray([-20.1, 52.4, 0, 0.05, 0.05, 49])
+        output_layer = numpy.asarray([-20.1, 52.4, 0, 0.05, 0.05, 49, 20, 20])
+        rng.set_state(rng_state)
+        sigmoid = Sigmoid(idim=input_layer.shape[0], odim=output_layer.shape[0], rng=rng)
+
+        # When
+        forward = sigmoid.fprop(input_layer)
+        expected = numpy.asarray([0.067, 0.728, 0.999, 0.512, 0.159, 0.584, 0.238, 0.932])
+
+        # Then
+        assert_array_almost_equal(forward, expected, decimal=3)
