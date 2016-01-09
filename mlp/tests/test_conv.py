844d03a258d7d0d014af83f59d7f55778b273239
Sam Davies <samdavies@live.co.uk>
2016-01-09 14:31:43 +0000

    Test full fprop

--- mlp/tests/test_conv.py
+++ mlp/tests/test_conv.py
@@ -24,8 +24,8 @@ class FeatureMapTestCase(TestCase):
         weights = conv.W[0]
         self.assertEqual(weights.shape, (24, 24, 5, 5))
 
-    def test_fprop(self):
-        """ Ensure that 1 forward prop pass works """
+    def test_fprop_for_1_iamge(self):
+        """ Ensure that 1 forward prop pass works for 1 image """
         conv = ConvLinear(1, 1,
                           image_shape=(28, 28),
                           kernel_shape=(5, 5),
@@ -50,5 +50,118 @@ class FeatureMapTestCase(TestCase):
         expected = numpy.zeros((num_rows_units, num_cols_units), dtype=numpy.float32)
         expected[0][0] = 1.0
         expected[23][23] = 1.0
-        actual = conv.fprop_single_feature_map([input], 0).reshape(24, 24)
+        actual = conv.fprop_single_feature_map([input], 0)
         assert_array_equal(actual, expected)
+
+    def test_fprop(self):
+        """ Ensure that 1 forward prop pass works """
+        conv = ConvLinear(1, 1,
+                          image_shape=(28, 28),
+                          kernel_shape=(5, 5),
+                          stride=(1, 1),
+                          irange=0.2,
+                          rng=None,
+                          conv_fwd=None,
+                          conv_bck=None,
+                          conv_grad=None)
+
+        conv.W = numpy.ones(conv.W.shape, dtype=numpy.float32)
+
+        # make an image of zeros with 1 in 2 corners
+        image = numpy.zeros((784), dtype=numpy.float32)
+        image[0] = 1.0
+        image[783] = 1.0
+
+        # make 1 feature map
+        feature_maps = numpy.array([image])
+
+        # make a batch of size 2
+        batch = numpy.array([feature_maps, feature_maps])
+
+        weights = conv.W[0]
+        num_rows_units = len(weights[0])
+        num_cols_units = len(weights[1])
+
+        expected = numpy.zeros((2, 1, num_rows_units, num_cols_units), dtype=numpy.float32)
+        expected[0][0][0][0] = 1.0
+        expected[0][0][23][23] = 1.0
+        expected[1][0][0][0] = 1.0
+        expected[1][0][23][23] = 1.0
+        actual = conv.fprop(batch)
+        assert_array_equal(actual, expected, verbose=True)
+
+    def test_fprop_from_2d_layer(self):
+        """ Ensure that 1 forward prop pass works """
+        conv = ConvLinear(1, 1,
+                          image_shape=(28, 28),
+                          kernel_shape=(5, 5),
+                          stride=(1, 1),
+                          irange=0.2,
+                          rng=None,
+                          conv_fwd=None,
+                          conv_bck=None,
+                          conv_grad=None)
+
+        conv.W = numpy.ones(conv.W.shape, dtype=numpy.float32)
+
+        # make an image of zeros with 1 in 2 corners
+        image = numpy.zeros((784), dtype=numpy.float32)
+        image[0] = 1.0
+        image[783] = 1.0
+
+        # make a batch of size 2
+        batch = numpy.array([image, image])
+
+        weights = conv.W[0]
+        num_rows_units = len(weights[0])
+        num_cols_units = len(weights[1])
+
+        expected = numpy.zeros((2, 1, num_rows_units, num_cols_units), dtype=numpy.float32)
+        expected[0][0][0][0] = 1.0
+        expected[0][0][23][23] = 1.0
+        expected[1][0][0][0] = 1.0
+        expected[1][0][23][23] = 1.0
+        actual = conv.fprop(batch)
+        assert_array_equal(actual, expected, verbose=True)
+
+    def test_fprop_many_out_feature_maps(self):
+        """ Ensure that 1 forward prop pass works with many out feature maps """
+        conv = ConvLinear(1, 2,
+                          image_shape=(28, 28),
+                          kernel_shape=(5, 5),
+                          stride=(1, 1),
+                          irange=0.2,
+                          rng=None,
+                          conv_fwd=None,
+                          conv_bck=None,
+                          conv_grad=None)
+
+        conv.W = numpy.ones(conv.W.shape, dtype=numpy.float32)
+
+        # make an image of zeros with 1 in 2 corners
+        image = numpy.zeros((784), dtype=numpy.float32)
+        image[0] = 1.0
+        image[783] = 1.0
+
+        # make 2 feature maps
+        feature_maps = numpy.array([image, image])
+
+        # make a batch of size 2
+        batch = numpy.array([feature_maps, feature_maps])
+
+        weights = conv.W[0]
+        num_rows_units = len(weights[0])
+        num_cols_units = len(weights[1])
+
+        expected = numpy.zeros((2, 2, num_rows_units, num_cols_units), dtype=numpy.float32)
+        expected[0][0][0][0] = 1.0
+        expected[0][0][23][23] = 1.0
+        expected[1][0][0][0] = 1.0
+        expected[1][0][23][23] = 1.0
+        # copy the previous feature map
+        expected[0][1] = expected[0][0]
+        expected[1][1] = expected[1][0]
+        actual = conv.fprop(batch)
+        assert_array_equal(actual, expected, verbose=True)
+
+
