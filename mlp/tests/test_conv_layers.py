9fcb20d46262faab81f36ff5a2c0f4f8575da539
Sam Davies <samdavies@live.co.uk>
2016-01-11 13:28:57 +0000

    Test extend conv to relu and sigmoid

--- mlp/tests/test_conv_layers.py
+++ mlp/tests/test_conv_layers.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import numpy
 import time
 
-from mlp.conv import ConvLinear
+from mlp.conv import ConvLinear, ConvSigmoid, ConvRelu
 from numpy.testing import assert_array_equal
 
 from mlp.costs import CECost, MSECost
@@ -11,6 +11,7 @@ from mlp.dataset import MNISTDataProvider
 from mlp.layers import MLP, Linear, Softmax, Sigmoid
 from mlp.optimisers import SGDOptimiser
 from mlp.schedulers import LearningRateFixed
+
 
 class LayerTypeTestCase(TestCase):
     def test_model_sigmoid(self):
@@ -22,10 +23,10 @@ class LayerTypeTestCase(TestCase):
 
         cost = CECost()
         model = MLP(cost=cost)
-        model.add_layer(ConvLinear(1, 1, image_shape=(28, 28), kernel_shape=(5, 5), stride=(1, 1), irange=0.2))
+        model.add_layer(ConvSigmoid(1, 1, image_shape=(28, 28), kernel_shape=(5, 5), stride=(1, 1), irange=0.2))
         model.add_layer(Softmax(idim=576, odim=10))
 
-        lr_scheduler = LearningRateFixed(learning_rate=0.5, max_epochs=2)
+        lr_scheduler = LearningRateFixed(learning_rate=0.5, max_epochs=3)
         optimiser = SGDOptimiser(lr_scheduler=lr_scheduler, dp_scheduler=None,
                                  l1_weight=0.0, l2_weight=0.0)
 
@@ -33,4 +34,26 @@ class LayerTypeTestCase(TestCase):
 
         tst_cost, tst_accuracy = optimiser.validate(model, test_dp)
 
-        self.assertAlmostEqual(tst_accuracy, 0.326, delta=0.005)
+        self.assertAlmostEqual(tst_accuracy, 0.086, delta=0.005)
+
+    def test_model_relu(self):
+        """ Ensure that back prop works with pgrads """
+        train_dp = MNISTDataProvider(dset='train', batch_size=100, max_num_batches=3, randomize=True)
+        valid_dp = MNISTDataProvider(dset='valid', batch_size=100, max_num_batches=3, randomize=True)
+        test_dp = MNISTDataProvider(dset='eval', batch_size=100, max_num_batches=3, randomize=True)
+        train_dp.reset()
+
+        cost = CECost()
+        model = MLP(cost=cost)
+        model.add_layer(ConvRelu(1, 1, image_shape=(28, 28), kernel_shape=(5, 5), stride=(1, 1), irange=0.2))
+        model.add_layer(Softmax(idim=576, odim=10))
+
+        lr_scheduler = LearningRateFixed(learning_rate=0.5, max_epochs=3)
+        optimiser = SGDOptimiser(lr_scheduler=lr_scheduler, dp_scheduler=None,
+                                 l1_weight=0.0, l2_weight=0.0)
+
+        optimiser.train(model, train_dp, valid_iterator=valid_dp)
+
+        tst_cost, tst_accuracy = optimiser.validate(model, test_dp)
+
+        self.assertAlmostEqual(tst_accuracy, 0.336, delta=0.005)
